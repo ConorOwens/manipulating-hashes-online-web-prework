9216c9a285e6265c50490b4b4d5983dabf371b1b
Conor Owens <conorj.owens@gmail.com>
2019-04-10 03:50:39 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -16,7 +16,7 @@ def first_challenge
 contacts.map do |person, data|
   data.map do |attribute, value|
     if attribute == :favorite_icecream_flavors
-      attribute.delete_if {|value| value == strawberry}
+      value.delete_if {|flavor| flavor == strawberry}
     end
   end
 end
